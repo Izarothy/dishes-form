7d327b73d6657f616f4a72bbd64449f2280e053e
Izarothy <izarothmail@gmail.com>
2022-06-22 19:41:32 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home-background': "url('/src/images/home-background.jpg')"
+      }
+    },
+  },
+  plugins: [],
+};
