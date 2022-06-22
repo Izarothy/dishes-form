27a68ef2476df7c6010fef69250c996b3865de5b
Izarothy <izarothmail@gmail.com>
2022-06-22 20:58:23 +0200

    Prep time format test, clean up the other tests

--- src/components/DishesForm.test.tsx
+++ src/components/DishesForm.test.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import DishesForm from './DishesForm';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
-test('Pizza is the default value for dish type', () => {
-  render(
+const setup = () => {
+  const utils = render(
     <Provider store={store}>
       <DishesForm />
     </Provider>
   );
+
+  return {
+    ...utils,
+  };
+};
+test('Pizza is the default value for dish type', () => {
+  setup();
+
   const soupText = screen.queryByText(/Scale/i);
   const sandwichText = screen.queryByText(/Bread/i);
 
@@ -18,15 +26,21 @@ test('Pizza is the default value for dish type', () => {
 });
 
 test('The basic fields are rendered', () => {
-  render(
-    <Provider store={store}>
-      <DishesForm />
-    </Provider>
-  );
+  setup();
 
   expect(screen.getByText(/Dish name/i)).toBeInTheDocument();
   expect(screen.getByText(/Preparation time/i)).toBeInTheDocument();
   expect(screen.getByText(/Dish type/i)).toBeInTheDocument();
   expect(screen.getByText(/Number of slices/i)).toBeInTheDocument();
   expect(screen.getByText(/Pizza diameter/i)).toBeInTheDocument();
+});
+
+test('An error if preparation time has a wrong format', () => {
+  setup();
+
+  const prepTimeInput = screen.getByLabelText(/Preparation time/i);
+
+  fireEvent.change(prepTimeInput, { target: { value: '0' } });
+
+  expect(screen.getByText(/Please use a hh:mm:ss format/i)).toBeInTheDocument();
 });
